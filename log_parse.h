aaab25108d8ca0a9eb6b4879966c4e8a99aa6663
Ralph Loader <ralph@i.geek.nz>
2008-04-23 14:57:25 +1200

    Comment.

--- log_parse.h
+++ log_parse.h
@@ -6,6 +6,9 @@
 
 struct database;
 
+/**
+ * Populate @c database from the given file @c f.  @c l and @c l_len are used
+ * for storing lines as they are read fromthe file.  */
 void read_files_versions (struct database * database,
                           char ** restrict l, size_t * l_len, FILE * f);
 
